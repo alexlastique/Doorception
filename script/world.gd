9518268573b5f0a0adbb0e91d68d55cf51bb87ae
Alexis Godet <alexis.godet05@gmail.com>
2024-04-16 21:00:33 +0200

    video fin

--- script/world.gd
+++ script/world.gd
@@ -59,19 +59,41 @@ func _change_map(destination_world : String):
 	add_child(current_world)
 
 
+var value
+var result
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if get_node("ath/ProgressBar").value == 00:
-		get_node("CanvasLayer/gameOver").show()
-		get_node("CanvasLayer/gameOver").play()
-		print("You Lose")
-		#get_tree().quit()
-	if int(get_node("ath/piece/Control/Label").text) >= 10:
-		get_node("CanvasLayer/win").show()
-		get_node("CanvasLayer/win").play()
-		print("You Win")
-		#get_tree().quit()
+	if get_node("ath/ProgressBar").value == 90:
+		value = "gameOver"
+	if int(get_node("ath/piece/Control/Label").text) >= 1:
+		value = "win"
+	if !result:
+		if value == "win":
+			get_node("CanvasLayer/win").show()
+			get_node("CanvasLayer/win").play()
+			result = true
+		elif value == "gameOver":
+			get_node("CanvasLayer/gameOver").show()
+			get_node("CanvasLayer/gameOver").play()
+			result = true
+		
 
 func _input(event : InputEvent):
 	if event.is_action_pressed("ui_home") and !optionsOpen and inGame:
 		gamePaused = !gamePaused
+
+
+func _on_win_finished():
+	if result:
+		get_tree().quit()
+
+
+func _on_game_over_finished():
+	if result:
+		current_world.queue_free()
+		result = false
+		value = ""
+		get_node("ath/ProgressBar").value = 100
+		get_node("ath/piece/Control/Label").text = "0"
+		get_node("CanvasLayer/gameOver").hide()
+		get_node("CanvasLayer/Menu").show()
